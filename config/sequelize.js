6a0eb5cb4e87e97cf47d797e8de3c36d90d6f04f
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-02-28 18:42:34 +0700

    bug fix

--- config/sequelize.js
+++ config/sequelize.js
@@ -16,14 +16,13 @@ switch (env) {
         });
         break;
     case `production`:
-        sequelize = new Sequelize(
-            process.env.DB_NAME,
-            process.env.DB_USER,
-            process.env.DB_PASSWORD,
-            {
-                host: process.env.DB_HOST,
-                port: process.env.DB_PORT,
-                dialect: 'postgres'
+        sequelize = new Sequelize({
+            database: process.env.DB_NAME,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT,
+            dialect: 'postgres'
             });
         break;
 }
