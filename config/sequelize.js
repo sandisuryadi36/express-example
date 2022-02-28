aeef985886a18d7f5a177a48b96c947cbfddda15
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-02-28 18:05:12 +0700

    add port

--- config/sequelize.js
+++ config/sequelize.js
@@ -11,12 +11,14 @@ switch (env) {
     case 'development':
         sequelize = new Sequelize('edu_sequelize', 'root', 'admin', {
             host: 'localhost',
+            port: 3306,
             dialect: `mysql` /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
         });
         break;
     case `production`:
         sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
             host: DB_HOST,
+            port: DB_PORT,
             dialect: 'postgres'
         });
         break;
