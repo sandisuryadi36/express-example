3e008edfc32b68c95454897fefd82017d69d6965
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-02-28 17:51:02 +0700

    setting production environtment

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,9 +1,25 @@
 // connect to sequelize
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('edu_sequelize', 'root', 'admin', {
-    host: 'localhost',
-    dialect: `mysql` /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
-});
+// get evirontment state
+const env = process.env.NODE_ENV || 'development';
+
+let sequelize
+
+// checking environtment
+switch (env) {
+    case 'development':
+        sequelize = new Sequelize('edu_sequelize', 'root', 'admin', {
+            host: 'localhost',
+            dialect: `mysql` /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+        });
+        break;
+    case `production`:
+        sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+            host: DB_HOST,
+            dialect: 'postgres'
+        });
+        break;
+}
 
 module.exports = sequelize;
