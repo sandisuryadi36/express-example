5225a0247afa8dec164dea6e77054a4d3e6e0393
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-02-28 15:52:17 +0700

    complete CRUD

--- apps/controllers/userController.js
+++ apps/controllers/userController.js
@@ -1,6 +1,37 @@
 const User = require('../models/userModel');
 
-const index = async (req, res) => {
+const index = (req, res) => { 
+    res.send({
+        message: "Welcome to User API",
+        status: "success",
+        endPoint: {
+            viewAll: {
+                method: "GET",
+                url: "/user"
+            },
+            create: {
+                method: "POST",
+                url: "/user"
+            },
+            update: {
+                method: "PUT",
+                url: "/user/:id",
+                params: {
+                    id: "id of user"
+                }
+            },
+            delete: {
+                method: "DELETE",
+                url: "/user/:id",
+                params: {
+                    id: "id of user"
+                }
+            }
+        }
+    });
+}
+
+const viewAll = async (req, res) => {
     try {
         // await User.sync();
         const data = await User.findAll();
@@ -29,7 +60,52 @@ const create = async (req, res) => {
     }
 }
 
+const update = async (req, res) => { 
+    const { id } = req.params;
+    const { firstName, lastName, age, job } = req.body;
+    try {
+        await User.sync();
+        const data = await User.update({
+            firstName,
+            lastName,
+            age,
+            job
+        }, {
+            where: {
+                id
+            }
+        })
+        res.status(200).send({
+            message: "Success",
+            data
+        });
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+const deleteUser = async (req, res) => { 
+    const { id } = req.params;
+    try {
+        await User.sync();
+        const data = await User.destroy({
+            where: {
+                id
+            }
+        })
+        res.status(200).send({
+            message: "Success",
+            data
+        });
+    } catch (error) {
+        res.send(error);
+    }
+}
+
 module.exports = {
     index,
-    create
+    viewAll,
+    create,
+    update,
+    deleteUser
 }
--- routes.js
+++ routes.js
@@ -2,8 +2,10 @@ const User = require('./apps/models/userModel');
 const Controller = require('./apps/controllers/userController');
 const router = require('express').Router();
 
-router.get("/", Controller.index)
+router.get('/', Controller.index)
+router.get("/user", Controller.viewAll)
 router.post("/user", Controller.create)
-
+router.put("/user/:id", Controller.update)
+router.delete("/user/:id", Controller.deleteUser)
 
 module.exports = router;
