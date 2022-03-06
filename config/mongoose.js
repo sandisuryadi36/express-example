943011a77a6eb96d08bbdfbe877c0c18d2b8bb71
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-03-06 13:08:35 +0700

    set up connection env

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,20 @@
 // setup mongoose connection
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://editor:12345@127.0.0.1/edu_mongoose?authSource=admin')
-const db = mongoose.connection;
+// get evirontment state
+const env = process.env.NODE_ENV || 'development';
+
+let db = new mongoose.Connection();
+switch (env) { 
+    case 'development':
+        mongoose.connect('mongodb://editor:12345@127.0.0.1/edu_mongoose?authSource=admin')
+        db = mongoose.connection
+        break
+    case 'production':
+        mongoose.connect(process.env.MONGODB_ADDON_URI)
+        db = mongoose.connection
+        break
+}
 
 db.once('open', () => { console.log('Connected to MongoDB') });
 db.on('error', (err) => { console.log(err) });
