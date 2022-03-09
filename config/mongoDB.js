bdd1cd00eb5e1533c26284f0262f1d268aaefe3d
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2022-03-09 22:40:36 +0700

    fix bug

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -1,23 +1,24 @@
-const { MongoClient } = require ("mongodb");
+const { MongoClient } = require("mongodb");
 
 // get evirontment state
 const env = process.env.NODE_ENV || 'development';
 
 // connect to database
-const url = () => { 
-    if (env === 'development') {
-        return 'mongodb://editor:12345@127.0.0.1/edu_mongoose?authSource=admin'
-    } else {
-        return process.env.MONGODB_ADDON_URI
+const url = () => {
+    switch (env) {
+        case 'development':
+            return 'mongodb://editor:12345@127.0.0.1/edu_mongoose?authSource=admin'
+        case 'production':
+            return process.env.MONGODB_ADDON_URI
     }
 }
 const client = new MongoClient(url(), { useNewUrlParser: true });
-client.connect(err => { 
+client.connect(err => {
     if (err) {
         console.log(err);
     } else console.log('Connected to MongoDB');
 })
 
-const db = client.db('edu_mongoose');
+const db = client.db();
 
 module.exports = db;
